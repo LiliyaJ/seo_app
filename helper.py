5e47c120597f5ffce00640f80ec1753e40cfb69e
Liliya Jeromin <zl108108@gmail.com>
2025-03-26 12:06:31 +0100

    pseudo code

---
+++ helper.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import os
+from data.client import RestClient
+
+
+# get data from googlesheet
+
+# prepare parameters for API call function
+
+# make an API call
+
+# transform data
+
+# send data back to googlesheet
