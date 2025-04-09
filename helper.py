5c42824fc5e2d3e5d2f1d5677d28491958af39cb
Liliya Jeromin <zl108108@gmail.com>
2025-04-09 14:35:15 +0200

    helper

--- helper.py
+++ helper.py
@@ -1,33 +1,14 @@
-import requests
-import json
-import os
-from data.client import RestClient
-from google.oauth2 import service_account
 import pandas as pd
-from googleapiclient.discovery import build
-from google.cloud import bigquery
 
 
-### for local debugging
-#Authentication with service account for bigquery
-credentials = service_account.Credentials.from_service_account_file(
-     'bq_service_account.json', scopes=[
-         "https://www.googleapis.com/auth/drive",
-         "https://www.googleapis.com/auth/cloud-platform"],
- )
 
-spreadsheetId = os.environ['spreadsheetId']
-project_id = os.environ['project_id']
-dataset_id = os.environ['dataset_id']
-table_id = os.environ['table_id']
+# # get data from googlesheet
+def get_data_from_gs (spreadsheet_id, service):
+    values = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range='A1:Z10000').execute()['values']
+    return pd.DataFrame(values[1:], columns = values[0])
 
-
-# get data from googlesheet
-
-# prepare parameters for API call function
 
 # make an API call
-
 def fetch_historical_search_volume(keyword_location_language_list, client):
     """
     Fetch historical search volume using DataForSEO API for specific keyword-location-language combinations.
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from google.oauth2 import service_account
 import pandas as pd
 from googleapiclient.discovery import build
 from google.cloud import bigquery
+from helper import get_data_from_gs, fetch_historical_search_volume
 
 
 ### for local debugging
@@ -16,13 +17,16 @@ credentials = service_account.Credentials.from_service_account_file(
          "https://www.googleapis.com/auth/cloud-platform"],
  )
 
-spreadsheetId = os.environ['spreadsheetId']
+spreadsheet_id = os.environ['spreadsheet_id']
 project_id = os.environ['project_id']
 dataset_id = os.environ['dataset_id']
 table_id = os.environ['table_id']
 
 DFS_LOGIN = os.environ['DFS_LOGIN']
-print(DFS_LOGIN)
 DFS_KEY = os.environ['DFS_KEY']
 
 client = RestClient(DFS_LOGIN, DFS_KEY)
+
+gs_data = get_data_from_gs (spreadsheet_id, build('sheets', 'v4', credentials=credentials))
+
+print(gs_data)
